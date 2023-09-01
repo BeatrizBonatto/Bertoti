788b93d553a3cbf5926ff41324dfa965307d447c
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2023-09-01 19:09:35 -0300

    Aula dia 28/08

---
+++ src/Almoxarifado.java
@@ -0,0 +1,28 @@
+package Almox;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Almoxarifado {
+    private List<Item> item = new LinkedList<Item>();
+    private List<Usuario> usuario = new LinkedList<Usuario>();
+
+    public void addItem(Item item){
+
+        livro.add(item);
+    }
+    public void addUsuario(Usuario usuario) {
+        usuario.add(usuario);
+    }
+    public List<Item>bucarItemNome(String nome);
+
+    List<Item> encontados = new LinkedList<Item>();
+    for(Item item: itens) {
+        if(item.getNome()equals(nome)encontrados.add(item));
+    }
+    return encontrados;
+}
+
+
+//uma lista vazia existe
+//uma lista nula não existe
